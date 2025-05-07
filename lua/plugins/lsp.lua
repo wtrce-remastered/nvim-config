0e195554a3ec61569f6a276a447199063293a5ae
wtrce-remastered <georgejornd@gmail.com>
2025-05-07 17:13:20 +0300

    fidget opacity down

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,80 +1,89 @@
 return {
     {
-        'neovim/nvim-lspconfig',
-        event = 'VeryLazy',
+        "neovim/nvim-lspconfig",
+        event = "VeryLazy",
         dependencies = {
-            { 'j-hui/fidget.nvim', opts = {} },
-            { 'folke/neodev.nvim', opts = {} }
+            {
+                "j-hui/fidget.nvim",
+                opts = {
+                    notification = {
+                        window = {
+                            winblend = 0
+                        }
+                    }
+                },
+            },
+            { "folke/neodev.nvim", opts = {} },
         },
         config = function()
-            local lspconfig = require('lspconfig')
+            local lspconfig = require("lspconfig")
 
             local lspconfig_defaults = lspconfig.util.default_config
             lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-                'force',
+                "force",
                 lspconfig_defaults.capabilities,
-                require('cmp_nvim_lsp').default_capabilities()
+                require("cmp_nvim_lsp").default_capabilities()
             )
 
-            vim.lsp.enable('lua_ls')
+            vim.lsp.enable("lua_ls")
 
-            vim.lsp.enable('eslint')
-            vim.lsp.enable('ts_ls')
-        end
+            vim.lsp.enable("eslint")
+            vim.lsp.enable("ts_ls")
+        end,
     },
-    { 'hrsh7th/cmp-nvim-lsp' },
+    { "hrsh7th/cmp-nvim-lsp" },
     {
-        'hrsh7th/nvim-cmp',
+        "hrsh7th/nvim-cmp",
         config = function()
-            local cmp = require('cmp')
+            local cmp = require("cmp")
 
             cmp.setup({
                 sources = {
-                    { name = 'nvim_lsp' }
+                    { name = "nvim_lsp" },
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-n>'] = cmp.mapping.complete {},
-                    ['<Tab>'] = cmp.mapping(function(fallback)
+                    ["<C-n>"] = cmp.mapping.complete({}),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             if cmp.get_selected_entry() ~= nil then
-                                cmp.mapping.confirm {
+                                cmp.mapping.confirm({
                                     behavior = cmp.ConfirmBehavior.Replace,
-                                }
+                                })
                             else
                                 cmp.select_next_item()
-                                cmp.mapping.confirm {
+                                cmp.mapping.confirm({
                                     behavior = cmp.ConfirmBehavior.Replace,
-                                }
+                                })
                             end
                         else
                             fallback()
                         end
-                    end, { 'i', 's' }),
-                    ['<CR>'] = cmp.mapping.confirm {
+                    end, { "i", "s" }),
+                    ["<CR>"] = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
-                    },
+                    }),
                 }),
                 snippet = {
                     expand = function(args)
                         vim.snippet.expand(args.body)
-                    end
-                }
+                    end,
+                },
             })
-        end
+        end,
     },
     {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         config = function()
-            require('mason').setup({})
-        end
+            require("mason").setup({})
+        end,
     },
     {
-        'williamboman/mason-lspconfig.nvim',
+        "williamboman/mason-lspconfig.nvim",
         config = function()
-            require('mason-lspconfig').setup {
-                automatic_enable = true
-            }
-        end
+            require("mason-lspconfig").setup({
+                automatic_enable = true,
+            })
+        end,
     },
 }
