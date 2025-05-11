156989aaf5682cc16be381a5e13bcb349a223733
wtrce-remastered <georgejornd@gmail.com>
2025-05-12 01:14:29 +0300

    harpoon keymap improvements

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,6 @@ return {
 
         vim.keymap.set("n", "<leader>jm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>jr", function() harpoon:list():remove() end)
         vim.keymap.set("n", "<leader>jclr", function() harpoon:list():clear() end)
 
         vim.keymap.set("n", "<C-e>", function() harpoon:list():select(1) end)
@@ -26,5 +25,13 @@ return {
         vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
         vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
         vim.keymap.set("n", "<leader>0", function() harpoon:list():select(0) end)
+
+        vim.keymap.set("n", "<leader>j4", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>j5", function() harpoon:list():replace_at(5) end)
+        vim.keymap.set("n", "<leader>j6", function() harpoon:list():replace_at(6) end)
+        vim.keymap.set("n", "<leader>j7", function() harpoon:list():replace_at(7) end)
+        vim.keymap.set("n", "<leader>j8", function() harpoon:list():replace_at(8) end)
+        vim.keymap.set("n", "<leader>j9", function() harpoon:list():replace_at(9) end)
+        vim.keymap.set("n", "<leader>j0", function() harpoon:list():replace_at(0) end)
     end
 }
