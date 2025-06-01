f24102ba5e24f0d7462668129b6ccee7483e3207
wtrce-remastered <georgejornd@gmail.com>
2025-06-01 18:15:23 +0300

    remove rest nvim

--- lua/plugins/rest-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "rest-nvim/rest.nvim",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        vim.keymap.set('n', '<leader>xr', ':Rest run<CR>')
-    end,
-}
