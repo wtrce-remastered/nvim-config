7850c1fe8aabe05ceb11673fd49366854cbc0bfa
wtrce remastered <georgejornd@gmail.com>
2025-07-23 22:54:21 +0300

    upd

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -43,11 +43,10 @@ vim.keymap.set('i', '<C-r>', function() vim.api.nvim_feedkeys('|', 'i', true) en
 
 vim.keymap.set('i', '<C-v>', function() vim.api.nvim_feedkeys('#', 'i', true) end, { silent = true })
 vim.keymap.set('i', '<C-d>', function() vim.api.nvim_feedkeys('.', 'i', true) end, { silent = true })
+
 vim.keymap.set('i', '<C-s>', function() vim.api.nvim_feedkeys('/', 'i', true) end, { silent = true })
 
 -- others
-
-vim.keymap.set('n', '<C-o>', '/')
 
 vim.keymap.set('n', '<leader>n', '_', { noremap = true })
 
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,9 @@ return {
     'akinsho/toggleterm.nvim',
     config = function()
         require("toggleterm").setup({
-            size = 15
+            size = 15,
+            persist_mode = true,
+            start_in_insert = true
         })
 
         vim.keymap.set("n", "<C-t>", ":ToggleTerm size=20 dir=$PWD direction=float<cr>", { silent = true })
