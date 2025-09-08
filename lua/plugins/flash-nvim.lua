de7f949eb23effeb91b94f550e820949ee465739
wtrce remastered <georgejornd@gmail.com>
2025-09-08 22:19:58 +0300

    flash nvim keys

--- lua/plugins/flash-nvim.lua
+++ lua/plugins/flash-nvim.lua
@@ -2,13 +2,18 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {
+        modes = {
+            char = {
+                keys = {},
+            }
+        },
         label = {
             exclude = "qjzkxwvzfgdyQJZKXWVZFGDY"
         }
     },
     keys = {
-        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
     },
 }
