051a82170e2bbd0d115b87f82107edb55a3789c1
wtrce remastered <georgejornd@gmail.com>
2025-11-21 02:15:18 +0300

    theme shoot

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,13 +1,22 @@
 return {
-    "gbprod/nord.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("nord").setup({
-            transparent = true,
-            borders = true,
-        })
-
-        vim.cmd.colorscheme("nord")
-    end,
+    "nyoom-engineering/oxocarbon.nvim",
+    config = function ()
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    end
 }
+
+-- return {
+--     "gbprod/nord.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("nord").setup({
+--             transparent = true,
+--             borders = true,
+--         })
+--
+--         vim.cmd.colorscheme("nord")
+--     end,
+-- }
