5590afd0cd7052a570d647931d0ca2d7b2141ebb
wtrce remastered <georgejornd@gmail.com>
2025-11-21 10:05:56 +0300

    kanso shoot

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,36 @@
 return {
-    'dasupradyumna/midnight.nvim',
+    "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
-    config = function ()
-        vim.cmd.colorscheme 'midnight'
+    config = function()
+        require('kanso').setup({
+            bold = true,                 -- enable bold fonts
+            italics = false,             -- enable italics
+            compile = false,             -- enable compiling the colorscheme
+            undercurl = true,            -- enable undercurls
+            commentStyle = { italic = false },
+            functionStyle = {},
+            keywordStyle = { italic = true},
+            statementStyle = {},
+            typeStyle = {},
+            transparent = true,         -- do not set background color
+            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+            colors = {                   -- add/modify theme and palette colors
+                palette = {},
+                theme = { zen = {}, pearl = {}, ink = {}, all = {} },
+            },
+            overrides = function(colors) -- add/modify highlights
+                return {}
+            end,
+            background = {               -- map the value of 'background' option to a theme
+                dark = "ink",           -- try "zen", "mist" or "pearl" !
+                light = "ink"           -- try "zen", "mist" or "pearl" !
+            },
+            foreground = "default",      -- "default" or "saturated" (can also be a table like background)
+        })
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-
-        vim.api.nvim_set_hl(0, "SignColumn",      { bg = "none" })
-
+        vim.cmd("colorscheme kanso")
     end
 }
 
