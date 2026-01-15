4d7625707195a1c4ae9c63b8dd2590b5aaa0b7d9
wtrce remastered <georgejornd@gmail.com>
2026-01-15 05:01:28 +0300

    harpoon menu second chance

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,34 +7,15 @@ return {
 
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>ha", function() harpoon:list():replace_at(11) end)
-        vim.keymap.set("n", "<leader>he", function() harpoon:list():replace_at(12) end)
-        vim.keymap.set("n", "<leader>hi", function() harpoon:list():replace_at(13) end)
+        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():add() end)
 
-        vim.keymap.set("n", "<C-a>", function() harpoon:list():select(11) end)
-        vim.keymap.set("n", "<C-e>", function() harpoon:list():select(12) end)
-        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(13) end)
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():replace_at(120) end)
+        vim.keymap.set("n", "<leader>he", function() harpoon:list():replace_at(121) end)
+        vim.keymap.set("n", "<leader>hi", function() harpoon:list():replace_at(122) end)
 
-        vim.keymap.set("n", "<leader>0", function() harpoon:list():select(0) end)
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
-        vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
-        vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
-        vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
-        vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
-
-        vim.keymap.set("n", "<leader>s0", function() harpoon:list():replace_at(0) end)
-        vim.keymap.set("n", "<leader>s1", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader>s2", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader>s3", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader>s4", function() harpoon:list():replace_at(4) end)
-        vim.keymap.set("n", "<leader>s5", function() harpoon:list():replace_at(5) end)
-        vim.keymap.set("n", "<leader>s6", function() harpoon:list():replace_at(6) end)
-        vim.keymap.set("n", "<leader>s7", function() harpoon:list():replace_at(7) end)
-        vim.keymap.set("n", "<leader>s8", function() harpoon:list():replace_at(8) end)
-        vim.keymap.set("n", "<leader>s9", function() harpoon:list():replace_at(9) end)
+        vim.keymap.set("n", "<C-a>", function() harpoon:list():select(120) end)
+        vim.keymap.set("n", "<C-e>", function() harpoon:list():select(121) end)
+        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(122) end)
     end
 }
